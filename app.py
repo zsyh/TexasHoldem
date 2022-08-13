74b802f55101949f29c47507d753eb748fb93ff3
zsyh <zsyh9612@gmail.com>
2022-08-13 17:50:13 +0800

    set debug to False

--- app.py
+++ app.py
@@ -207,4 +207,4 @@ def on_all_in(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app=app, host='0.0.0.0', port=5128, debug=True)
+    socketio.run(app=app, host='0.0.0.0', port=5128, debug=False)
